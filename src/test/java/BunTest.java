07de5e4c9e99b8f98490ee86178d7830b4e76f29
Karina Palanchuk <lbmmrt@yandex.ru>
2022-02-22 12:53:10 +0300

    added unit tests

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -4,30 +4,25 @@ import org.mockito.Mock;
 import praktikum.Bun;
 import praktikum.Database;
 
-import java.util.Random;
-
 import static org.junit.Assert.assertEquals;
 
 public class BunTest {
 
     private Bun bun;
 
-    @Mock
-    Database database = new Database();
-
     @Test
     public void getName() {
-        bun = database.availableBuns().get(0);
+        bun = new Bun("red bun", 300);
         String actual = bun.getName();
-        String expected = "black bun";
+        String expected = "red bun";
         assertEquals(expected, actual);
     }
 
     @Test
     public void getPrice() {
-        bun = database.availableBuns().get(1);
+        bun = new Bun("black bun", 100);
         float actual = bun.getPrice();
-        float expected = 200f;
+        float expected = 100f;
         Assert.assertEquals(expected, actual, 0.0);
     }
 }
